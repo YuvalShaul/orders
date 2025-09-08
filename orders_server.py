1efa0cefccf7eba56e6528b954192cf273555cc9
Yuval Shaul <yuval.shaul@gmail.com>
2025-09-08 14:33:44 -0400

    adding OK

--- orders_server.py
+++ orders_server.py
@@ -15,7 +15,8 @@ def main_error():
 @app.route('/orders', methods=['GET'])
 def get_all_orders():
     # return all_orders
-    return fake_orders
+    print("ok")
+    return all_orders
 
 @app.route('/orders', methods=['POST'])
 def add_new_order():     #  Details: "prod", "quantity", but no id
